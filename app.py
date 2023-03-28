69a119b19fb17d2ca2639c3d7e13b0e00b988601
Paulo Vinicius Gomes da Silva <paulovinicius1732@gmail.com>
2023-03-28 14:59:24 -0300

    Layout Finished and Flask Configuration

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
     return render_template("index.html")
+
+
+@app.route("/knowledge")
+def knowledge():
+    return render_template("knowledge.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
 
